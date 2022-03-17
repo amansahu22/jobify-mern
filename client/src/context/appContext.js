ae964a784389c57eff88d8a22f73ca2e72870207
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2022-03-17 23:17:24 +0530

    Navbar, small sidebar, big Sidebar finished

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,129 +1,153 @@
 import React, { useReducer, useContext } from "react";
-import axios from 'axios';
+import axios from "axios";
 import reducer from "./reducer";
-import { DISPLAY_ALERT, CLEAR_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_SUCCESS, REGISTER_USER_ERROR, LOGIN_USER_BEGIN, LOGIN_USER_SUCCESS, LOGIN_USER_ERROR } from "./actions";
+import {
+  DISPLAY_ALERT,
+  CLEAR_ALERT,
+  REGISTER_USER_BEGIN,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+  LOGIN_USER_BEGIN,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
+  TOGGLE_SIDEBAR,
+  LOGOUT_USER,
+} from "./actions";
 
-const user = localStorage.getItem('user')
-const token = localStorage.getItem('token')
-const location = localStorage.getItem('location')
-
+const user = localStorage.getItem("user");
+const token = localStorage.getItem("token");
+const location = localStorage.getItem("location");
 
 const initialState = {
-    isLoading: false,
-    isAlertShown: false,
-    alertText: '',
-    alertType: '',
-    token: token,
-    user: user ? JSON.parse(user) : null,
-    userLocation: location || '',
-    jobLocation: location || '',
-}
+  isLoading: false,
+  isAlertShown: false,
+  alertText: "",
+  alertType: "",
+  token: token,
+  user: user ? JSON.parse(user) : null,
+  userLocation: location || "",
+  jobLocation: location || "",
+  showSidebar: false,
+};
 
 const AppContext = React.createContext();
 
 const AppContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const [state, dispatch] = useReducer(reducer, initialState);
+  const toggleSidebar = () => {
+    dispatch({ type: TOGGLE_SIDEBAR });
+  };
 
-    const displayAlert = () => {
-        dispatch({ type: DISPLAY_ALERT });
-        clearAlert()
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+    clearAlert();
+  };
+
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({ type: CLEAR_ALERT });
+    }, 3000);
+  };
+
+  const addDataToLocalStorage = (data) => {
+    const { user, token, location } = data;
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("location", location);
+  };
+
+  const removeDataFromLocalStorage = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    localStorage.removeItem("location");
+  };
+
+  const loginUser = async (currentUser) => {
+    dispatch({ type: LOGIN_USER_BEGIN });
+    try {
+      const response = await axios.post("/api/v1/auth/login", currentUser);
+      const { user, token, location } = response.data;
+      dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: {
+          user,
+          token,
+          location,
+        },
+      });
+
+      addDataToLocalStorage({ user, token, location });
+    } catch (error) {
+      console.log(error.response.data);
+      dispatch({
+        type: LOGIN_USER_ERROR,
+        payload: {
+          msg: error.response.data,
+        },
+      });
     }
 
-    const clearAlert = () => {
-        setTimeout(() => {
-            dispatch({ type: CLEAR_ALERT })
-        }, 3000)
+    clearAlert();
+  };
+
+  const registerUser = async (currentUser) => {
+    dispatch({ type: REGISTER_USER_BEGIN });
+
+    try {
+      const response = await axios.post("/api/v1/auth/register", currentUser);
+
+      // console.log(response.data)
+      const { user, token, location } = response.data;
+      dispatch({
+        type: REGISTER_USER_SUCCESS,
+        payload: {
+          user,
+          token,
+          location,
+        },
+      });
+
+      //after updating our state we will save same thing in local storage as well so that on refresh we do not loose data
+      addDataToLocalStorage({ user, token, location });
+    } catch (error) {
+      // console.log(error.response);
+      dispatch({
+        type: REGISTER_USER_ERROR,
+        payload: {
+          msg: error.response.data.msg,
+        },
+      });
     }
 
-    const addDataToLocalStorage = (data) => {
-        const { user, token, location } = data;
-        localStorage.setItem('user', JSON.stringify(user));
-        localStorage.setItem('token', token);
-        localStorage.setItem('location', location);
-    }
+    clearAlert(); //invoking clear alert because we are setting isAlertShown to true in both try and catch block
+  };
 
-    const removeDataFromLocalStorage = () => {
-        localStorage.removeItem('user');
-        localStorage.removeItem('token');
-        localStorage.removeItem('location');
-    }
+  const logoutUser = () => {
+    dispatch({ type: LOGOUT_USER });
+    removeDataFromLocalStorage();
+  };
 
-    const loginUser = async (currentUser) => {
-
-        dispatch({ type: LOGIN_USER_BEGIN })
-        try {
-            const response = await axios.post('/api/v1/auth/login', currentUser);
-            const { user, token, location } = response.data;
-            dispatch({
-                type: LOGIN_USER_SUCCESS,
-                payload: {
-                    user,
-                    token,
-                    location
-                }
-            })
-
-            addDataToLocalStorage({ user, token, location })
-        } catch (error) {
-            console.log(error.response.data);
-            dispatch({
-                type: LOGIN_USER_ERROR,
-                payload: {
-                    msg: error.response.data
-                }
-            })
-        }
-
-        clearAlert()
-    }
-
-    const registerUser = async (currentUser) => {
-        dispatch({ type: REGISTER_USER_BEGIN });
-
-        try {
-            const response = await axios.post('/api/v1/auth/register', currentUser);
-
-            // console.log(response.data)
-            const { user, token, location } = response.data;
-            dispatch({
-                type: REGISTER_USER_SUCCESS,
-                payload: {
-                    user,
-                    token,
-                    location,
-                },
-            })
-
-            //after updating our state we will save same thing in local storage as well so that on refresh we do not loose data
-            addDataToLocalStorage({ user, token, location })
-
-        } catch (error) {
-            // console.log(error.response);
-            dispatch({
-                type: REGISTER_USER_ERROR,
-                payload: {
-                    msg: error.response.data.msg
-                }
-            })
-        }
-
-        clearAlert() //invoking clear alert because we are setting isAlertShown to true in both try and catch block
-    }
-
-    return (
-        <AppContext.Provider value={{ ...state, displayAlert, registerUser, loginUser }}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        displayAlert,
+        registerUser,
+        loginUser,
+        toggleSidebar,
+        logoutUser,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 
 //creating custom hook so that in every place i do not need to import and execute useContext hook
 
 const useAppContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
 export { initialState, AppContextProvider, useAppContext };
-
-
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,88 +1,117 @@
-import { DISPLAY_ALERT, CLEAR_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_SUCCESS, REGISTER_USER_ERROR, LOGIN_USER_BEGIN, LOGIN_USER_SUCCESS, LOGIN_USER_ERROR } from "./actions";
+import {
+  DISPLAY_ALERT,
+  CLEAR_ALERT,
+  REGISTER_USER_BEGIN,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+  LOGIN_USER_BEGIN,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
+  TOGGLE_SIDEBAR,
+  LOGOUT_USER,
+} from "./actions";
+import { initialState } from "./appContext";
 const reducer = (state, action) => {
+  if (action.type === DISPLAY_ALERT) {
+    return {
+      ...state,
+      isAlertShown: true,
+      alertText: "Please provide all values",
+      alertType: "danger",
+    };
+  }
 
-    if (action.type === DISPLAY_ALERT) {
-        return {
-            ...state,
-            isAlertShown: true,
-            alertText: 'Please provide all values',
-            alertType: 'danger',
-        }
-    }
+  if (action.type === CLEAR_ALERT) {
+    return {
+      ...state,
+      isAlertShown: false,
+      alertText: "",
+      alertType: "",
+    };
+  }
 
-    if (action.type === CLEAR_ALERT) {
-        return {
-            ...state,
-            isAlertShown: false,
-            alertText: '',
-            alertType: ''
-        }
-    }
+  if (action.type === REGISTER_USER_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
 
-    if (action.type === REGISTER_USER_BEGIN) {
-        return {
-            ...state,
-            isLoading: true
-        }
-    }
+  if (action.type === REGISTER_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      isAlertShown: true,
+      alertText: "User Created Successfully!! redirecting...",
+      alertType: "success",
+      token: action.payload.token,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+    };
+  }
 
-    if (action.type === REGISTER_USER_SUCCESS) {
-        return {
-            ...state,
-            isLoading: false,
-            isAlertShown: true,
-            alertText: 'User Created Successfully!! redirecting...',
-            alertType: 'success',
-            token: action.payload.token,
-            user: action.payload.user,
-            userLocation: action.payload.location,
-            jobLocation: action.payload.location,
-        }
-    }
+  if (action.type === REGISTER_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      isAlertShown: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
+    };
+  }
 
-    if (action.type === REGISTER_USER_ERROR) {
-        return {
-            ...state,
-            isLoading: false,
-            isAlertShown: true,
-            alertType: 'danger',
-            alertText: action.payload.msg,
-        }
-    }
+  if (action.type === LOGIN_USER_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
 
-    if (action.type === LOGIN_USER_BEGIN) {
-        return {
-            ...state,
-            isLoading: true,
-        }
-    }
+  if (action.type === LOGIN_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      token: action.payload.token,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      isAlertShown: true,
+      alertType: "success",
+      alertText: "Logged in Successfully!! redirecting....",
+    };
+  }
 
-    if (action.type === LOGIN_USER_SUCCESS) {
-        return {
-            ...state,
-            isLoading: false,
-            token: action.payload.token,
-            user: action.payload.user,
-            userLocation: action.payload.location,
-            jobLocation: action.payload.location,
-            isAlertShown: true,
-            alertType: 'success',
-            alertText: 'Logged in Successfully!! redirecting....',
-        }
-    }
+  if (action.type === LOGIN_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      isAlertShown: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
+    };
+  }
 
-    if (action.type === LOGIN_USER_ERROR) {
-        return {
-            ...state,
-            isLoading: false,
-            isAlertShown: true,
-            alertType: 'danger',
-            alertText: action.payload.msg
-        }
-    }
+  if (action.type === TOGGLE_SIDEBAR) {
+    return {
+      ...state,
+      showSidebar: !state.showSidebar,
+    };
+  }
 
+  if (action.type === LOGOUT_USER) {
+    return {
+      ...initialState,
+      //in this case only we are returning null because if user logged out we simply wanna go back to our default state
+      //but we are overwriting below states manually because these values are were in localState and removing localState data does not change the state.
+      user: null,
+      token: null,
+      userLocation: null,
+      jobLocation: null,
+    };
+  }
 
-    throw new Error(`No Such Action: ${action.type}`)
-}
+  throw new Error(`No Such Action: ${action.type}`);
+};
 
 export default reducer;
