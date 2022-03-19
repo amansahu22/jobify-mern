61a5588ad127d78e13bac6653682d25f44af4acc
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2022-03-20 00:13:14 +0530

    Job Model created and create job implemented

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,21 +1,37 @@
-const createJob = (req, res) => {
-    res.send('create job')
-}
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+
+const createJob = async (req, res) => {
+  const { company, position } = req.body;
+
+  if (!company || !position) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  req.body.createdBy = req.user.userId;
+  //remember at the time we are validating our token we added user field to our req body
+
+  const job = await Job.create(req.body);
+
+  console.log(job);
+  res.status(StatusCodes.CREATED).json(job);
+};
 
 const deleteJob = (req, res) => {
-    res.send('delete job')
-}
+  res.send("delete job");
+};
 
 const getAllJobs = (req, res) => {
-    res.send('get all jobs')
-}
+  res.send("get all jobs");
+};
 
 const updateJob = (req, res) => {
-    res.send('update job')
-}
+  res.send("update job");
+};
 
 const showStats = (req, res) => {
-    res.send('show stats')
-}
+  res.send("show stats");
+};
 
-export { createJob, deleteJob, getAllJobs, updateJob, showStats }
+export { createJob, deleteJob, getAllJobs, updateJob, showStats };
